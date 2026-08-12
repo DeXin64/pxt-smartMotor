ca4001ee59d858188ea0c9167199c02a1f36c759
uio64 <2692571183@qq.com>
2026-08-12 15:43:58 +0800

    加大力矩

--- main.ts
+++ main.ts
@@ -383,8 +383,8 @@ namespace smartMotor {
                     i2cCommandSend(COMMAND_STOP, [robotMotorMask()])
                     usingPulseApproach = true
                 }
-                sendMotorRelativeStep(robotLeftMotor, ROBOT_TURN_PULSE_STEP_DEGREES, 20)
-                sendMotorRelativeStep(robotRightMotor, ROBOT_TURN_PULSE_STEP_DEGREES, 20)
+                sendMotorRelativeStep(robotLeftMotor, ROBOT_TURN_PULSE_STEP_DEGREES, 30)
+                sendMotorRelativeStep(robotRightMotor, ROBOT_TURN_PULSE_STEP_DEGREES, 30)
             } else {
                 let signedTurnSpeed = positiveDirection ? turnSpeed : -turnSpeed
                 sendRobotSpeed(signedTurnSpeed, -signedTurnSpeed)
